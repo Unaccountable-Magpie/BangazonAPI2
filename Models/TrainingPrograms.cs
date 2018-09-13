2d5b51ff04ac8dab3e1821a9ff717833157e04de
AmandaM04 <wreedamitchell@gmail.com>
2018-09-13 10:40:36 -0500

    made model for Training Programs

---
+++ Models/TrainingPrograms.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class TrainingPrograms
+    {
+        [Key]
+        public int TrainingProgramsId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public string ProgramName { get; set; }
+        public string MaxAttendees { get; set; }
+
+        public List<TrainingPrograms> TrainingProgramsList = new List<TrainingPrograms>();
+
+        public IEnumerable<TrainingPrograms> TrainingProgram;
+    }
+}
