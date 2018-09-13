f409cb3e4a9e3a8e23486d82a176cf4f437fe48f
unknown <austin.gorman@gmail.com>
2018-09-13 10:10:42 -0500

    Added models for customers, departments, employees, orders, and product orders

---
+++ Models/Orders.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BangazonAPI.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int sId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public int CustomersId { get; set; }
+        public Customers Customers { get; set; }
+        public int? PaymentTypesId { get; set; } // ? means that the variable can be null
+        public PaymentTypes PaymentTypes { get; set; }
+
+        public IEnumerable<OrderProduct> ProductOrders;
+
+    }
+}
---
+++ Models/ProductOrders.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BangazonAPI.Models
+{
+    public class ProductOrders
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OrdersId { get; set; }
+        public Orders Orders { get; set; }
+
+        [Required]
+        public int ProductsId { get; set; }
+        public Products Products { get; set; }
+    }
+}
