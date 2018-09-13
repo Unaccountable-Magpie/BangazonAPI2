08aa031a9c4b69b6b9d93c0ca52870b5ac763e28
AmandaM04 <wreedamitchell@gmail.com>
2018-09-13 14:55:31 -0500

    fixed the Id to only say Id instead of TrainingProgramsId

--- Models/TrainingPrograms.cs
+++ Models/TrainingPrograms.cs
@@ -10,7 +10,7 @@ namespace BangazonAPI.Models
     public class TrainingPrograms
     {
         [Key]
-        public int TrainingProgramsId { get; set; }
+        public int Id { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
