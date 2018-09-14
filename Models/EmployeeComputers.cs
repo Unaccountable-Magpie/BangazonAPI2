cd395f5bf1f8d68e08a7ba5bce5d4a7c05136c7e
Jewel Ramirez <chimpy4u@yahoo.com>
2018-09-14 09:27:00 -0700

    Adding employee computer model

---
+++ Models/EmployeeComputers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class EmployeeComputers
+    {
+        [Key]
+        public int EmployeeComputersId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime AssignmentStartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime AssignmentEndDate { get; set; }
+
+        [Required]
+        public int EmployeesId { get; set; }
+        public Employees Employees { get; set; }
+        public int ComputersId { get; set; } // ? means that the variable can be null
+        public Computers Computers { get; set; }
+    }
+}
