27015c943fff89c21ab30f767a4508ae7572f8b7
Lauren Richert <lsrichert@gmail.com>
2018-09-14 11:06:18 -0500

    added required documentation to the EmployeeTrainingPrograms model

--- Models/EmployeeTrainingPrograms.cs
+++ Models/EmployeeTrainingPrograms.cs
@@ -4,6 +4,9 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
+//Author: Lauren Richert
+//This class references a join table which references the Employees table and the TrainingPrograms table.
+
 namespace BangazonAPI.Models
 {
     public class EmployeeTrainingPrograms
