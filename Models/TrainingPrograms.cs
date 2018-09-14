1c32df9602798a1a170f79d31cd8643e2ecd1a2e
AmandaM04 <wreedamitchell@gmail.com>
2018-09-14 15:35:08 -0500

    added notation for the reason of using attributes

--- Models/TrainingPrograms.cs
+++ Models/TrainingPrograms.cs
@@ -1,6 +1,10 @@
 ﻿//Purpose: refrences the TrainingPrograms table in the database. Gets and Sets data that the associated 
 // controller will be able to access and use to make the program run.
 //Author: Amanda Mitchell
+//Models: Used [Key] - unique value for this particular entity
+//  [Required] - data is required to make the controllers work. 
+/*  [DataType(DataType.Date)]  - signifies that the data is something different than the standard and inside the (),
+you are letting it know that the datatype is date*/
 
 using System;
 using System.Collections.Generic;
