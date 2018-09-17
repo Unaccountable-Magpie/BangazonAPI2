f56b0a4ec37b40519d2699e1db006b2b22d48028
Lauren Richert <lsrichert@gmail.com>
2018-09-17 15:17:15 -0500

    took out attributes

--- Models/EmployeeTrainingPrograms.cs
+++ Models/EmployeeTrainingPrograms.cs
@@ -11,14 +11,14 @@ namespace BangazonAPI.Models
 {
     public class EmployeeTrainingPrograms
     {
-        [Key]
+        
         public int Id { get; set; }
 
-        [Required]
+       
         public int EmployeesId { get; set; }
         public Employees Employees { get; set; }
 
-        [Required]
+        
         public int TrainingProgramsId { get; set; }
         public TrainingPrograms TrainingPrograms { get; set; }
     }
