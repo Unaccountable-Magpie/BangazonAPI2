f0540305899e92d6df738a7cc242b6387614994b
Lauren Richert <lsrichert@gmail.com>
2018-09-17 14:40:14 -0500

    took out the attributes

--- Models/EmployeeTrainingPrograms.cs
+++ Models/EmployeeTrainingPrograms.cs
@@ -7,18 +7,18 @@ using System.Threading.Tasks;
 //Author: Lauren Richert
 //This class references a join table which references the Employees table and the TrainingPrograms table.
 
-namespace BangazonAPI.Models
+namespace BangazonAPI.Controllers
 {
     public class EmployeeTrainingPrograms
     {
-        [Key]
+        
         public int Id { get; set; }
 
-        [Required]
+        
         public int EmployeesId { get; set; }
         public Employees Employees { get; set; }
 
-        [Required]
+        
         public int TrainingProgramsId { get; set; }
         public TrainingPrograms TrainingPrograms { get; set; }
     }
