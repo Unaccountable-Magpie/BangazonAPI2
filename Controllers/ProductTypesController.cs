21e6bdd0d9be80872ab471d5ff313fac32ecb7f8
AmandaM04 <wreedamitchell@gmail.com>
2018-09-17 11:23:23 -0500

    post working

--- Controllers/ProductTypesController.cs
+++ Controllers/ProductTypesController.cs
@@ -44,7 +44,7 @@ namespace BangazonAPI.Controllers
         }
 
         // GET: api/ProductTypes/3
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}", Name = "GetProductTypes")]
         public async Task<IActionResult> Get([FromRoute]int id)
         {
             using (IDbConnection conn = Connection)
@@ -58,14 +58,50 @@ namespace BangazonAPI.Controllers
 
         // POST: api/ProductTypes
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] ProductTypes ProductTypes)
         {
+            string sql = $@"INSERT INTO ProductTypes
+                (Name)
+                VALUES
+                ('{ProductTypes.Name}');
+                select MAX(Id) from ProductTypes";
+
+            using (IDbConnection conn = Connection)
+            {
+                var NewProductTypesId = (await conn.QueryAsync<int>(sql)).Single();
+                ProductTypes.Id = NewProductTypesId;
+                return CreatedAtRoute("GetProductTypes", new { id = NewProductTypesId }, ProductTypes);
+            }
         }
 
         // PUT: api/ProductTypes/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put([FromRoute]int id,  ProductTypes ProductTypes)
         {
+            string sql = $@"
+            UPDATE ProductTypes
+            SET Name = '{ProductTypes.Name}'
+            WHERE Id = {id}";
+
+            try
+            {
+                using (IDbConnection conn = Connection)
+                {
+                    int rowsAffected = await conn.ExecuteAsync(sql);
+                    if (rowsAffected > 0)
+                    {
+                        return new StatusCodeResult(StatusCodes.Status204NoContent);
+                    }
+                    throw new Exception("No rows affected");
+                }
+            }
+            catch (Exception)
+            {
+                if (!PaymentTypesExists(id))
+                {
+
+                }
+            }
         }
 
         // DELETE: api/ApiWithActions/5
